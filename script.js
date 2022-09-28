527fd4051fcee8819c626f39cfc7268b656c191e
Krzysztof Gruca <kgruca21@gmail.com>
2022-09-27 22:47:44 -0700

    coding challenge #4, question 5

--- script.js
+++ script.js
@@ -828,6 +828,11 @@ console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
 console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
 // logs Alice and Bob and Michael's dogs eat too little!
 
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+// logs false
+
+
 /*
 const dogs = [
   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
