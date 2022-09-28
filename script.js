f2cb2438d00ba6219754122bd89d3bb190b94127
Krzysztof Gruca <kgruca21@gmail.com>
2022-09-27 22:08:55 -0700

    coding challenge #4, question 1

--- script.js
+++ script.js
@@ -767,7 +767,10 @@ Julia and Kate are still studying dogs, and this time they are studying if dogs 
 Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
 Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
 
-1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. 
+
+Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+
 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
@@ -779,7 +782,10 @@ Eating an okay amount means the dog's current food portion is within a range 10%
 HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
 HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
 
-TEST DATA:
+
+GOOD LUCK 😀
+*/
+
 const dogs = [
   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
   { weight: 8, curFood: 200, owners: ['Matilda'] },
@@ -787,8 +793,12 @@ const dogs = [
   { weight: 32, curFood: 340, owners: ['Michael'] }
 ];
 
-GOOD LUCK 😀
-*/
+// 1.
+dogs.forEach(dog => dog.recommendedFood = Math.floor((dog.weight 
+  ** 0.75 * 28)));
+
+// 2. 
+
 
 /*
 const dogs = [
